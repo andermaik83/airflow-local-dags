1f8149306db64fa76c683247dd92f99555ad18cf
Ander Lopetegui <ander.lopetegui@clarivate.com>
2025-10-16 10:23:54 +0200

    update

--- bigb_opsi_monitor_dag.py
+++ bigb_opsi_monitor_dag.py
@@ -31,6 +31,6 @@ with DAG(
     opsi_monitor_task = WinRMOperator(
         task_id='run_opsi_monitor',
         command=f'cd /d E:\\Local\\OPSi_monitor && {SCRIPT_PATH} 1>{LOG_DIR}\\output.log 2>{LOG_DIR}\\error.log',
-        ssh_conn_id=WINRM_CONN_ID      
+        winrm_hook=winRMHook      
     )
 
