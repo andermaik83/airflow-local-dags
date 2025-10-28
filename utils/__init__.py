419f53c4e0508612ab1dcbf087c8974b89c98b7a
Ander Lopetegui <ander.lopetegui@clarivate.com>
2025-10-28 23:17:53 +0100

    update

--- utils/__init__.py
+++ utils/__init__.py
@@ -9,34 +9,34 @@ Modules:
 
 Usage:
     # Import command generators and connection constants
-    from utils import get_file_check_command, SSHConnections
+    from utils import check_file, SSHConnections
     
     # Use with SSHOperator
     SSHOperator(
         task_id='check_file',
         ssh_conn_id=SSHConnections.TGEN_VL105,
-        command=get_file_check_command('/TEST/SHR/ALBA/work/ALBA_imgissue.par')
+        command=check_file('/TEST/SHR/ALBA/work/ALBA_imgissue.par')
     )
 """
 
 # Export SSH command generators and utilities
 from .common_utils import (
     get_environment_from_path,
-    get_file_check_command,
-    get_file_pattern_check_command,
-    get_directory_check_command,
-    get_file_size_command,
-    get_wait_for_stable_file_command,
+    check_file,
+    check_file_pattern,
+    check_directory,
+    get_file_size,
+    wait_for_stable_file,
     SSHConnections
 )
 
 # Export only the new command generator functions
 __all__ = [
     'get_environment_from_path',
-    'get_file_check_command',
-    'get_file_pattern_check_command',
-    'get_directory_check_command', 
-    'get_file_size_command',
-    'get_wait_for_stable_file_command',
+    'check_file',
+    'check_file_pattern',
+    'check_directory', 
+    'get_file_size',
+    'wait_for_stable_file',
     'SSHConnections'
 ]
